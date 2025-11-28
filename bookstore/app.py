a524a26731e738cfdd0e996aa6f650422e7aad92
Sophia Yan <141109786+TaroSy214@users.noreply.github.com>
2025-11-27 20:46:25 -0500

    Update app to use environment variable for port

--- bookstore/app.py
+++ bookstore/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
-
+import os
 
 # instantiate the app
 app = Flask(__name__)
@@ -80,5 +80,7 @@ def handle_error(e):
     return render_template('error.html', error=e)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
